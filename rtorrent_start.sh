719636e22d4de0c039bb38bc0b9afd22f92a1c13
Maxim Kremenev <ezotrank@gmail.com>
2012-01-12 00:51:01 +0200

    fix rtorrent

--- rtorrent_start.sh
+++ rtorrent_start.sh
@@ -24,11 +24,11 @@ function check_ssh {
 function main {
     # If of each is true we stopped rtorrent. Else we start it )
     if check_live_hosts || check_ssh ; then
-	transmission-remote -U -D
-	echo "Unlimite torrent $(date)"
-    else
 	transmission-remote -u 0 -d 0
 	echo "Limited torrent $(date)"
+    else
+	transmission-remote -U -D
+	echo "Unlimite torrent $(date)"
     fi
 }
 
